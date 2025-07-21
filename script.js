a6b63b589dd63918de78c3daab259dd9cc7733f8
kryoblue <118422761+kryoblue@users.noreply.github.com>
2025-07-21 11:47:25 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,3 @@
+<script src="https://cdn.jsdelivr.net/npm/@tensorflow/tfjs@latest"></script>
+<script src="https://cdn.jsdelivr.net/npm/@tensorflow-models/pose-detection"></script>
+<script src="./script.js"></script> <!-- Your own script -->
